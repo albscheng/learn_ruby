9b8fd9dbfb2d8f8e64555670eeb4d689a40a0524
Albert Cheng <albscheng@gmail.com>
2016-06-10 00:41:58 -0700

    finished some of 03

---
+++ 03_simon_says/simon_says.rb
@@ -0,0 +1,29 @@
+def echo(words)
+    words
+end
+
+def shout(words)
+    words.upcase
+end
+
+def repeat(words, n=2)
+    str = words
+    (n-1).times do
+        str = str + " " + words
+    end
+    str
+end
+
+def start_of_word(str, num)
+
+end
+
+def first_word(str)
+
+end
+
+def titleize(title)
+
+end
+
+

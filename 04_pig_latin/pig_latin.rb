d7fffbc85b016f811775c78722f8b805bb330719
acheng <albert.cheng@viasat.com>
2016-06-10 16:30:57 -0700

    finished assignment 04

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -4,16 +4,29 @@ def translate(sentence)
             "#{word}aeiou".index(vowel)
         }.min]
 
-        if phoneme.include? "q"
+        if phoneme.include?("q") || phoneme.include?("Q")
             if word[phoneme.length] == "u"
                 phoneme += "u"
             end
         end
-        "#{word[phoneme.length..-1]}#{phoneme}ay"
+
+        if phoneme.length > 0 && is_upper?(phoneme[0..0])
+            "#{word[phoneme.length..-1]}#{phoneme}ay".capitalize!
+        elsif word.length > 1 && !is_alpha?(word[-1])
+            punc = word[-1]
+            "#{word[phoneme.length..-2]}#{phoneme}ay" + punc
+        else
+            "#{word[phoneme.length..-1]}#{phoneme}ay"
+        end
     }.join(" ")
     latin
 end
 
+def is_upper?(c)
+    c == c.upcase
+end
 
-
+def is_alpha?(str)
+  !str.match(/[^A-Za-z]/)
+end
 
